36b1711991decc22658941bb6fd757390b26b2a9
zach.gharst <zdgharst@gmail.com>
2021-08-24 14:12:26 -0500

    Fix formatting mistake in exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,15 +6,15 @@ const Statistic = ({ label, value })  => <p>{label}: {value}</p>
 const App = () => {
   const [good, setGood]       = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad]        = useState(0)
+  const [bad, setBad]         = useState(0)
 
   const incrementGood    = () => setGood(good + 1)
   const incrementNeutral = () => setNeutral(neutral + 1)
   const incrementBad     = () => setBad(bad + 1)
 
   const calculateTotal    = good + neutral + bad
-  const calculateAverage  = calculateTotal == 0 ? '' : (good - bad) / calculateTotal
-  const calculatePositive = calculateTotal == 0 ? '' : good / calculateTotal + '%'
+  const calculateAverage  = calculateTotal === 0 ? '' : (good - bad) / calculateTotal
+  const calculatePositive = calculateTotal === 0 ? '' : good / calculateTotal + '%'
 
   return (
     <>
