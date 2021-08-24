ce307a2cf23ea2da655450107aa0ee925ebdae53
zach.gharst <zdgharst@gmail.com>
2021-08-24 14:15:27 -0500

    Complete exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,6 +3,24 @@ import React, { useState } from 'react'
 const Button    = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const Statistic = ({ label, value })  => <p>{label}: {value}</p>
 
+const Statistics = ({ good, neutral, bad }) => {
+  const calculateTotal    = good + neutral + bad
+  const calculateAverage  = calculateTotal === 0 ? '' : (good - bad) / calculateTotal
+  const calculatePositive = calculateTotal === 0 ? '' : good / calculateTotal + '%'
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <Statistic label='Good'     value={good} />
+      <Statistic label='Neutral'  value={neutral} />
+      <Statistic label='Bad'      value={bad} />
+      <Statistic label='Total'    value={calculateTotal} />
+      <Statistic label='Average'  value={calculateAverage} />
+      <Statistic label='Positive' value={calculatePositive} />
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood]       = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -12,9 +30,6 @@ const App = () => {
   const incrementNeutral = () => setNeutral(neutral + 1)
   const incrementBad     = () => setBad(bad + 1)
 
-  const calculateTotal    = good + neutral + bad
-  const calculateAverage  = calculateTotal === 0 ? '' : (good - bad) / calculateTotal
-  const calculatePositive = calculateTotal === 0 ? '' : good / calculateTotal + '%'
 
   return (
     <>
@@ -25,13 +40,7 @@ const App = () => {
       <Button onClick={incrementNeutral} text='Neutral' />
       <Button onClick={incrementBad}     text='Bad' />
 
-      <h2>Statistics</h2>
-      <Statistic label='Good'     value={good} />
-      <Statistic label='Neutral'  value={neutral} />
-      <Statistic label='Bad'      value={bad} />
-      <Statistic label='Total'    value={calculateTotal} />
-      <Statistic label='Average'  value={calculateAverage} />
-      <Statistic label='Positive' value={calculatePositive} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
