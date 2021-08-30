a55519c7df3555f13b18486bdf07338947bdccbb
zach.gharst <zdgharst@gmail.com>
2021-08-30 11:05:47 -0500

    Refactor note to component

--- part2/practice/notes/src/App.js
+++ part2/practice/notes/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
+import Note from './components/Note'
 
-const App = (props) => {
-  const { notes } = props
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => <li>{note.content}</li>)}
-      </ul>
-    </div>
-  )
-}
+const App = ({ notes }) => (
+  <div>
+    <h1>Notes</h1>
+    <ul>
+      {notes.map(note =>
+        <Note key={note.id} note={note} />
+      )}
+    </ul>
+  </div>
+)
 
 export default App
