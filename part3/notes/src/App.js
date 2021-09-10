363c51614dd13e2513ec52d767877986f69fb6a2
zach.gharst <zdgharst@gmail.com>
2021-09-10 14:03:20 -0500

    Add more notes practice

--- part3/notes/src/App.js
+++ part3/notes/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
   }
 
   const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
+    const url = `/api/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
   
