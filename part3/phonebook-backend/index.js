77d77c0da4dc40b1cfb9cf101e53e2792208fac1
zach.gharst <zdgharst@gmail.com>
2021-09-13 14:18:25 -0500

    Complete exercise 3.19

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -43,7 +43,7 @@ app.get('/api/persons/:id', (req, res, next) => {
   }).catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if(!body.name || !body.number) {
@@ -52,12 +52,6 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  // if(persons.find(person => person.name === body.name)) {
-  //   return res.status(400).json({ 
-  //     error: 'name must be unique' 
-  //   })
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number
@@ -65,7 +59,7 @@ app.post('/api/persons', (req, res) => {
 
   person.save().then(savedPerson => {
     res.json(savedPerson)
-  })
+  }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
@@ -104,6 +98,8 @@ const errorHandler = (error, req, res, next) => {
 
   if(error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return res.status(400).json( { error: error.message })
   }
 
   next(error)
