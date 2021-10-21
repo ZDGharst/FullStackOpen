4ad1491fd96cd5d5fa4b3797b1462a81da944485
zach.gharst <zdgharst@gmail.com>
2021-10-21 12:01:47 -0500

    Complete exercise 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,30 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Books = ({ books }) => (
-  <div>
-    <h2>books</h2>
+const Books = ({ books }) => {
+  const [genreFilter, setGenreFilter] = useState('')
+  const [genres, setGenres] = useState([])
 
-    <table>
-      <tbody>
-        <tr>
-          <th></th>
-          <th>
-            author
-          </th>
-          <th>
-            published
-          </th>
-        </tr>
-        {books.map(a =>
-        <tr key={a.title}>
-          <td>{a.title}</td>
-          <td>{a.author.name}</td>
-          <td>{a.published}</td>
-        </tr>
-        )}
-      </tbody>
-    </table>
-  </div>
-)
+  useEffect(() => {
+    const genreDict = {}
+    books.forEach(b => b.genres.forEach(g =>
+      genreDict[g] = true
+    ))
+
+    let genreBuilder = []
+    for(const key in genreDict) {
+      genreBuilder.push(key)
+    }
+    genreBuilder.sort()
+
+    setGenres(genreBuilder)
+  }, [books])
+
+  return (
+    <div>
+      <h2>books</h2>
+
+      <table style={{ marginBottom: 10 }}>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              author
+            </th>
+            <th>
+              published
+            </th>
+          </tr>
+          {books
+            .filter(b => !genreFilter || b.genres.includes(genreFilter))
+            .map(b =>
+              <tr key={b.title}>
+                <td>{b.title}</td>
+                <td>{b.author.name}</td>
+                <td>{b.published}</td>
+              </tr>
+            )}
+        </tbody>
+      </table>
+      <button onClick={() => setGenreFilter('')}>all genres</button>
+      {genres.map(g =>
+        <button onClick={() => setGenreFilter(g)} style={{ marginLeft: 5 }}>
+          {g}
+        </button>)
+      }
+    </div>
+  )
+}
 
 export default Books
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -14,6 +14,7 @@ query {
       name
     }
     published
+    genres
   }
 }
 `
