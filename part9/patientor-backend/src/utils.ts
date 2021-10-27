9d2c5fc0709eacebfc884a694692a50db8c60a12
zach.gharst <zdgharst@gmail.com>
2021-10-27 18:23:51 -0500

    Complete exercise 9.13

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,4 +1,4 @@
-import { NewPatient } from "./types";
+import { Gender, NewPatient } from "./types";
 
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
@@ -10,6 +10,20 @@ const parseField = (field: unknown, fieldName: string): string => {
   }
 
   return field;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGender = (gender: any): gender is Gender => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return Object.values(Gender).includes(gender);
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if(!gender || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender.');
+  }
+
+  return gender;
 };
 
 const toNewPatient = ({ name, ssn, dateOfBirth, occupation, gender } : {
@@ -24,7 +38,7 @@ const toNewPatient = ({ name, ssn, dateOfBirth, occupation, gender } : {
     ssn: parseField(ssn, 'ssn'),
     dateOfBirth: parseField(dateOfBirth, 'dateOfBirth'),
     occupation: parseField(occupation, 'occupation'),
-    gender: parseField(gender, 'gender')
+    gender: parseGender(gender)
   };
 
   return newPatient;
