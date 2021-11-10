f5141c6ceeb3b6f4b81fe2ba2799764fb75d6b0e
zach.gharst <zdgharst@gmail.com>
2021-11-10 13:00:15 -0600

    Refactor and complete exercise 10.7

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -4,7 +4,6 @@ import Constants from 'expo-constants';
 
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
-import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
