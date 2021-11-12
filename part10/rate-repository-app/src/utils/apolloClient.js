66fedd92bf79ea1420da80d381cc431b908acfa7
zach.gharst <zdgharst@gmail.com>
2021-11-11 19:48:17 -0600

    Complete exercise 10.12

--- part10/rate-repository-app/src/utils/apolloClient.js
+++ part10/rate-repository-app/src/utils/apolloClient.js
@@ -1,8 +1,10 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
+import Constants from 'expo-constants';
+
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://192.168.1.104:5000/graphql',
+  uri: Constants.manifest.extra.apollo_uri,
 });
 
 const createApolloClient = () => {
