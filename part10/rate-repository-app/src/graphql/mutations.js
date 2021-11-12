f0d2a0176e6ad01ab841b5b57306a0506bb7c1ff
zach.gharst <zdgharst@gmail.com>
2021-11-11 22:47:12 -0600

    Complete exercise 10.15

--- part10/rate-repository-app/src/graphql/mutations.js
+++ part10/rate-repository-app/src/graphql/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export const SIGN_IN = gql`
-mutation SignIn($username: String!, $password: String!) {
+mutation($username: String!, $password: String!) {
   authorize(credentials: { username: $username, password: $password }) {
     accessToken
   }
